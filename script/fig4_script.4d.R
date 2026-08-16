cde5e792b163122055572077edbee70c42dbdd88
songeric1107 <songeric1107@gmail.com>
2026-08-16 11:03:32 -0400

    Refactor fig4_script.4d.R for new data processing
    Updated the script to read and process new data, including renaming cell types and adjusting the output file.

--- script/fig4_script.4d.R
+++ script/fig4_script.4d.R
@@ -1,56 +1,9 @@
-dc=readRDS("integrated.DC.only.proportion.rds")
-DefaultAssay(dc)="RNA"
-dc1 <- DietSeurat(dc, assays = "RNA")
-#combined <- merge(
-# x = epi,
-#y = dc1
-# add.cell.ids = c("s1", "s2", "s3", "s4"))
-dc1=readRDS("MonoDC_dc.only.rds")
-dc2=readRDS("dc.singlet.rds")
-
-dc1 <- SCTransform(dc1, vst.flavor = "v2", verbose = FALSE)
-dc2 <- SCTransform(epi, vst.flavor = "v2", verbose = FALSE)
-
-data1=RunPCA(dc1,npcs = 30, verbose = FALSE)
-data2=RunPCA(dc2,npcs = 30, verbose = FALSE)
 
 
-data.list <- list(data1, data2)
-features <- SelectIntegrationFeatures(object.list = data.list, nfeatures = 3000)
+obj=readRDS("rename_epi.dc.330.2026(input_4d).rds")
+obj$celltype.final=gsub("epi_LYZ_Low","Epithelial 2",obj$celltype.final)
 
-data.list[[1]]$sample_id <- "dc"
-data.list[[2]]$sample_id <- "epi"
-
-data.list <- PrepSCTIntegration(object.list = data.list, anchor.features = features, verbose = TRUE)
-
-# Step 4: Find integration anchors
-anchors <- FindIntegrationAnchors(object.list = data.list, normalization.method = "SCT", 
-                                  anchor.features = features, verbose = TRUE)
-
-# Step 5: Integrate the datasets
-integrated <- IntegrateData(anchorset = anchors, normalization.method = "SCT", verbose = TRUE)
-
-# Step 6: Proceed with downstream analysis on integrated data
-# Set the default assay to the integrated data
-DefaultAssay(integrated) <- "integrated"
-
-# Run PCA
-integrated <- RunPCA(integrated, verbose = TRUE)
-
-# Run UMAP for visualization
-integrated <- FindNeighbors(integrated, dims = 1:30)
-
-integrated <- RunUMAP(integrated, dims = 1:30, verbose = TRUE)
-
-integrated <- FindClusters(integrated, resolution = 1.0, graph.name = "integrated_snn")
-# Find neighbors and clusters
-
-saveRDS(integrated,"integrated.epi.dc.2026.rds")
-
-integrated=readRDS("rename_epi.dc.330.2026.rds")
-integrated$celltype.final=gsub("epi_LYZ_Low","Epithelial 2",integrated$celltype.final)
-
-integrated$celltype.final=gsub("epi_FABP_LYZ_High","Epithelial 1",integrated$celltype.final)
+obj$celltype.final=gsub("epi_FABP_LYZ_High","Epithelial 1",obj$celltype.final)
 
 
 pdf("fig.s4.marker.pdf",15,10)
@@ -65,7 +18,7 @@ FeaturePlot_scCustom(integrated,features=gene)
 dev.off()
 
 
-saveRDS(integrated,"input_fig4d.rds")
+#saveRDS(integrated,"input_fig4d.rds")
 
 
 
